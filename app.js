f4e34a14894201b8c268ccf4a0c53cb379cdfc06
Gabriel Morris <gabemorris@outlook.com>
2018-08-27 11:44:34 -0700

    Extract rendering functions to their own module

--- app.js
+++ app.js
@@ -1,69 +1,11 @@
-/* global state */
+/* global state, Render */
 
-/* global state */
-
-/***** Render functions *****/
-// Function for initializing the board on page load
-function initialBoardRender() {
-  // Board grid HTML to insert
-  const boardHTML = `
-  <div class="row">
-  <div class="cell" id="0">
-    <p>&nbsp;</p>
-  </div>
-  <div class="cell" id="1">
-    <p>&nbsp;</p>
-  </div>
-  <div class="cell" id="2">
-    <p>&nbsp;</p>
-  </div>
-  </div>
-  <div class="row">
-    <div class="cell" id="3">
-      <p>&nbsp;</p>
-    </div>
-    <div class="cell" id="4">
-      <p>&nbsp;</p>
-    </div>
-    <div class="cell" id="5">
-      <p>&nbsp;</p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="cell" id="6">
-      <p>&nbsp;</p>
-    </div>
-    <div class="cell" id="7">
-      <p>&nbsp;</p>
-    </div>
-    <div class="cell" id="8">
-      <p>&nbsp;</p>
-    </div>
-  </div>
-  `;
-  // Insert into board
-  $('.board').html(boardHTML);
-}
-
-// Function for rendering the updated grid based on the state
-function renderUpdatedGrid() {
-  // Class for winning
-  const winClass = 'win';
-  // Array of jQuery objects of all cells on board
-  const arrOfCells = $('.cell');
-
-  // Loop through each cell and update their classes
-  arrOfCells.each(function(index) {
-    // If state contains a win pattern we will add the win class to each of the cells
-    if (state.winPattern.includes(index)) {
-      $(this).addClass(winClass);
-    }
-    // Update the cell with the strings from the state grid
-    $(this)
-      .children()
-      .html(state.grid[index]);
-  });
-}
+/*
+  Modules:
+  /app.js - event listeners and on ready function
+  /scripts/Render.js - functions for updating renders on page
+  /scripts/state.js - datastore and functions for updating state
+*/
 
 /***** Event Listeners *****/
 // Function for initializing all event listeners
@@ -71,7 +13,7 @@ function initializeEventListeners() {
   // Delegated listener for cell clicks
   $('.board').on('click', '.cell', function() {
     state.updateStoreGrid($(this));
-    renderUpdatedGrid();
+    Render.renderUpdatedGrid();
   });
   // Listener for new game button
   $('#new-game').on('click', function() {
@@ -82,7 +24,7 @@ function initializeEventListeners() {
 // Function for when DOM is ready
 function onReady() {
   // Initialize the board and event listeners
-  initialBoardRender();
+  Render.initialBoardRender();
   initializeEventListeners();
 }
 // On ready
---
+++ scripts/Render.js
@@ -0,0 +1,68 @@
+/* global state */
+
+/***** Render functions *****/
+const Render = (function() {
+  // Function for initializing the board on page load
+  const initialBoardRender = function() {
+    // Board grid HTML to insert
+    const boardHTML = `
+  <div class="row">
+  <div class="cell" id="0">
+    <p>&nbsp;</p>
+  </div>
+  <div class="cell" id="1">
+    <p>&nbsp;</p>
+  </div>
+  <div class="cell" id="2">
+    <p>&nbsp;</p>
+  </div>
+  </div>
+  <div class="row">
+    <div class="cell" id="3">
+      <p>&nbsp;</p>
+    </div>
+    <div class="cell" id="4">
+      <p>&nbsp;</p>
+    </div>
+    <div class="cell" id="5">
+      <p>&nbsp;</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="cell" id="6">
+      <p>&nbsp;</p>
+    </div>
+    <div class="cell" id="7">
+      <p>&nbsp;</p>
+    </div>
+    <div class="cell" id="8">
+      <p>&nbsp;</p>
+    </div>
+  </div>
+  `;
+    // Insert into board
+    $('.board').html(boardHTML);
+  };
+
+  // Function for rendering the updated grid based on the state
+  function renderUpdatedGrid() {
+    // Class for winning
+    const winClass = 'win';
+    // Array of jQuery objects of all cells on board
+    const arrOfCells = $('.cell');
+
+    // Loop through each cell and update their classes
+    arrOfCells.each(function(index) {
+      // If state contains a win pattern we will add the win class to each of the cells
+      if (state.winPattern.includes(index)) {
+        $(this).addClass(winClass);
+      }
+      // Update the cell with the strings from the state grid
+      $(this)
+        .children()
+        .html(state.grid[index]);
+    });
+  }
+
+  return { renderUpdatedGrid, initialBoardRender };
+})();
